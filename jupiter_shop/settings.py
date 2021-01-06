9300a013d958caf809ab1f0154fd8a4d982922c8
NFEL <filsaraee.27@gmail.com>
2021-01-06 08:39:20 +0330

    changed setting

--- jupiter_shop/settings.py
+++ jupiter_shop/settings.py
@@ -109,6 +109,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -270,12 +271,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
-]
-MEDIA_ROOT = BASE_DIR / 'media/'
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+import  dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
