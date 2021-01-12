cbb8900a187d1b36ad2821e690e16e28e74f5614
navid <nfilsaraee@gmail.com>
2021-01-13 00:09:20 +0330

    30

--- address/admin.py
+++ address/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from leaflet.admin import LeafletGeoAdmin
+
+
 from .models import Address
 
-admin.site.register(Address)
+admin.site.register(Address,LeafletGeoAdmin)
--- jupiter_shop/address/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Address(models.Model):
-    address=models.CharField(max_length=120)
-    city=models.CharField(max_length=30)
