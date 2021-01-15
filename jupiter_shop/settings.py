55c078db809affe7b4373e4e80b78773260cf07a
NFEL <filsaraee.27@gmail.com>
2021-01-15 21:37:31 +0330

    10

--- jupiter_shop/settings.py
+++ jupiter_shop/settings.py
@@ -232,7 +232,7 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# import  dj_database_url
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
-# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+import  dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
