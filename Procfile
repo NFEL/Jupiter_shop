105971896cb46cf045e8c8491cdcf7df9cd51200
NFEL <filsaraee.27@gmail.com>
2021-01-16 20:06:43 +0330

    forgot url and linking

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-release: python manage.py makemigrations
-release: python manage.py migrate
 web: gunicorn jupiter_shop.wsgi:application --log-file - --log-level debug
--- pardakht/urls.py
+++ pardakht/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import add_to_cart,remove_from_cart,reduction_from_cart,set_qty_in_cart,CartDetailView
+from .views import add_to_cart,remove_from_cart,reduction_from_cart,set_qty_in_cart,CartDetailView,AddressListView
 
 urlpatterns = [
     path('add/<int:id>', add_to_cart, name='add-to-cart'),
     path('sub/<int:id>', reduction_from_cart, name='reduce-from-cart'),
     path('set/<int:id>', set_qty_in_cart, name='set-qty-in-cart'),
     path('remove/<int:id>', remove_from_cart, name='remove-from-cart'),
+
+    path('checkout/', AddressListView.as_view(), name='checkout'),
     path('', CartDetailView.as_view(), name='cart'),
 ]
